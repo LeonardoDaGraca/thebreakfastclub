de211338182484a8fc98dad911af3e5523f9e26b
Lambientan <25370052+pedernal@users.noreply.github.com>
2023-01-29 16:59:24 -0500

    innitial commit

--- api/controllers/user-controller.js
+++ api/controllers/user-controller.js
@@ -1,10 +1,26 @@
-const getUser = async (req, res) => {
+/**
+* Pedro Gutierrez Rincon
+* 01/29/2023
+* the controllers for CRUD opperations are defined here.
+* Called by router.
+**/
+
+/*const getUser = async (req, res) => {
   return res.status(200).json({
     success: true,
     message: 'API is working.'
   });
+}*/
+
+async function getTest(req, res)
+{
+  let payload = {
+    success: true,
+    message: 'API is working (test)'
+  };
+  return res.status(200).json(payload);
 }
 
 module.exports = {
-  getUser,
+  getTest
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,45 @@
+/**
+* Pedro Gutierrez Rincon
+* 01/29/2023
+* Here the patterns for the REST API are defined and what controller should act
+* respectively.
+
+THE API:
+  Pattern for patients CRUD opperations:
+"GET http://<host>:<port>/api/patients" <-- gets all patients
+"GET http://<host>:<port>/api/patients/<id>" <-- gets patient with given ID
+"POST http://<host>:<port>/api/patients" <-- inserts a new defined patient into the collection
+"PUT http://<host>:<port>/api/patients/<id>" <-- updates the patient that corresponds to the given id
+"DELETE http://<host>:<port>/api/patients/<id>" <-- deletes the patient with the corresponding id
+
+  Pattern for exams CRUD opperations:
+"GET http://<host>:<port>/api/exams" <-- gets all exams
+"GET http://<host>:<port>/api/patients/<id>/exams" <-- returns all the exams that correspond to the given patient's id
+"GET http://<host>:<port>/api/exams" <-- gets an exam with a specific id
+"POST http://<host>:<port>/api/exams" <-- inserts a new defined exam into the collection
+"PUT http://<host>:<port>/api//<id>" <-- updates the exam that corresponds to the given id
+"DELETE http://<host>:<port>/api/exams/<id>" <-- deletes the exam with the conrresponding id
+
+  Pattern for other CRUD opperations:
+"GET http://<host>:<port>/api/geninfo" <-- gets the aggregation of general info of the patients and their exams
+**/
+
+/*FIXME:
+requires mongoose
+requires importing mongodb models for patients and exams
+requires express-async-handler
+*/
+
 var express = require('express');
 var router = express.Router();
 
+const UserController = require("../controllers/user-controller");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('API is working properly!');
-});
+});*/
+
+router.get('/', UserController.getTest);
 
 module.exports = router;
