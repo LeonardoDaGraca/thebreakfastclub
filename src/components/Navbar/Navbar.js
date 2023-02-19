ff4bcde814de232f1138d2452cfefef9be16518a
Salmonte <almontesauel@gmail.com>
2023-02-19 18:06:49 -0500

    Updated Admin Page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars, FaTimes, FaSearch } from 'react-icons/fa';
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,6 +23,25 @@ export const Navbar = () => {
           >
             MedBay
           </NavLink>
+          <div class="flex items-center w-1/2 justify-center relative md:hidden">
+            {/* <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            </div> */}
+            <input type="search" id="default-search" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Exams" required />
+            {/* <FaBeer class="text-white font-bold absolute right-0.5 bottom-2.5  focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-full text-2xl px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"/> */}
+            <button type="submit" class="text-white absolute right-0.5  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><FaSearch/></button>
+          </div>
+         
+            {/* <div className="flex justify-between items-center border-2 h-7 w-56 border-green-500 md:hidden">
+                    <input className="border-2 w-56 rounded-lg focus:outline-none text-xs md:p-2 relative"
+                        type="text" placeholder="Search" />
+
+                    <FaBeer className="flex text-red-500 absolute items-end" />
+              
+              
+                {/* <button className="absolute bg-gray-700 px-2 rounded-full text-zinc-50 font-bold h-auto">Search</button> */}
+            {/* </div> */}
+          
           <div className="-mr-2 flex md:hidden">
             <button
               onClick={handleToggle}
@@ -35,6 +54,7 @@ export const Navbar = () => {
               )}
             </button>
           </div>
+          
           <div className="hidden md:block">
             <div className="ml-4 flex items-center space-x-4 ">
               <NavLink
--- src/components/Search/TestSearch.js
+++ src/components/Search/TestSearch.js
@@ -2,9 +2,10 @@ export const TestSearch = () => {
     return (
         <>
          {/* Search bar */}
-         <div className="flex relative search-container justify-center p-6">
-            <div className="flex justify-between w-72 border-2">
-                <input className="border-2 p-2 rounded-lg focus:outline-none" type="text" placeholder="Search" />
+         <div className="md:flex relative search-container justify-center hidden">
+            <div className="flex justify-between md:w-72">
+                    <input className="border-2 rounded-lg focus:outline-none text-xs md:p-2"
+                        type="text" placeholder="Search" />
                 
                 <button className="bg-gray-700 p-2 rounded-lg text-zinc-50 font-bold">Search</button>
             </div>
