3a3fb9d9ef2089c6f13766f867ebb9459a31c242
Salmonte <almontesauel@gmail.com>
2023-02-19 18:09:38 -0500

    Updated Admin Page

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -29,7 +29,7 @@ export const Navbar = () => {
             </div> */}
             <input type="search" id="default-search" class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Exams" required />
             {/* <FaBeer class="text-white font-bold absolute right-0.5 bottom-2.5  focus:ring-4 focus:outline-none focus:ring-blue-300  rounded-full text-2xl px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"/> */}
-            <button type="submit" class="text-white absolute right-0.5  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><FaSearch/></button>
+            <button type="submit" class="text-white absolute right-1.5  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-2 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><FaSearch/></button>
           </div>
          
             {/* <div className="flex justify-between items-center border-2 h-7 w-56 border-green-500 md:hidden">
