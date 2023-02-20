f0222e1f049ee80b9ba07fe8ca0078438e098660
Salmonte <almontesauel@gmail.com>
2023-02-19 23:26:02 -0500

    Updated Admin Page

--- src/components/Create/TestForm.js
+++ src/components/Create/TestForm.js
@@ -7,7 +7,7 @@ export const TestForm = () => {
             <Navbar/>
             
             <div className="flex justify-center p-2 mt-10 md:mt-36 md:my-10  md:mx-auto ">
-                <form className="p-3 space-y-4 rounded-lg shadow-xl border md:shadow-blue-900 bg-gray-100 md:w-3/4" action="">
+                <form className="p-4 space-y-4 rounded-lg shadow-2xl border-2 md:border md:shadow-blue-900 bg-gray-100 md:w-3/4 md:p-5" action="">
                     <h1 className="mb-2 text-black text-base md:text-xl lg:text-2xl font-bold ">Create Exam</h1>
 
                     <div className="space-y-3 md:flex md:space-y-0">
@@ -59,7 +59,7 @@ export const TestForm = () => {
                             <label className="text-sm w-24 md:text-base md:w-24 lg:text-xl" for="sex">Sex</label>
                             <input className="border-2 border-gray-200 focus:outline-none lg:text-xl" type="text" name="sex" id="sex" />
                         </div>
-                        <div className="flex  items-center md:w-1/2">
+                        <div className="flex items-center md:w-1/2">
                             <label className="text-sm w-24 md:text-base md:w-24 lg:text-xl" for="zip-code">Zip Code</label>
                             <input className="border-2 lg:text-xl" type="text" name="zip-code" id="zip-code" />
                         </div>
