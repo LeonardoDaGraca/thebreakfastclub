0184ee40115a0864cab2bcb592bfd01b1188a57a
dow-mp <derin.mitchell@gmail.com>
2023-02-27 22:30:43 -0500

    update app.js imports after editing route loaders in index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import "./Styles/Styles.css"
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { Exams } from './components/Exams/Exams';
-import { Admin } from './components/Admin/Admin';
+// import { Admin } from './components/Admin/Admin';
 import { Footer } from "./components/Footer/Footer";
-import { useLoaderData } from "react-router-dom";
+// import { useLoaderData } from "react-router-dom";
 
 const App = () => {
-  const data = useLoaderData();
+  // const data = useLoaderData();
 
   // const handleDelete = (id) => {
   //   const examArr = JSON.parse(localStorage.getItem('localData'));
