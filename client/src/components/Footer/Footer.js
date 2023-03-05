b9d2d703d2bb9b637eccf47d334132a322829438
dow-mp <derin.mitchell@gmail.com>
2023-03-05 12:56:13 -0500

    update copyright to get current year

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 export const Footer = () => {
+let date = new Date();
+let year = date.getFullYear();
+
     return (
        
         <footer className="p-3 w-full  md:flex md:items-center md:justify-center md:p-4 bg-gray-900 fixed bottom-0">
-            <span className=" text-zinc-50 text-center text-xs md:text-sm"> © 2023 All Rights Reserved.
+            <span className=" text-zinc-50 text-center text-xs md:text-sm"> © {year} All Rights Reserved.
             </span>  
         </footer>
 
