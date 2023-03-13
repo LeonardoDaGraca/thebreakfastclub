3ee48515c6575d082c29288e1c5155f6a99d112d
Salmonte <almontesauel@gmail.com>
2023-03-12 23:54:15 -0400

    Updating componenets after pull and merge

--- client/src/components/Cards/CardData.js
+++ client/src/components/Cards/CardData.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineEdit, AiOutlineClose } from "react-icons/ai";
+import {GrClose} from "react-icons/gr"
+// import { AiOutlineEdit, AiOutlineClose } from "react-icons/ai";
 import Pagination from "../Pagination/Pagination";
 import { ExamDataPopUp } from "../PopUp/ExamDataPopUp";
 
@@ -65,7 +66,7 @@ export const CardData = ({data}) => {
             {selectedPatientId && (
                 <div className="fixed flex flex-col justify-center items-center top-0 left-0 w-full h-full bg-gray-800 bg-opacity-30 backdrop-blur-sm lg:px-16 xl:px-16 2xl:px-20">
                     <div className=" bg-white rounded-xl shadow-lg p-4 overflow-y-auto w-11/12 lg:w-full h-1/2">
-                        <div className="flex items-center justify-end  md:px-4 md:pt-4">
+                        <div className="flex items-center justify-end xl:p-2 2xl:p-6">
                             {/* <button */}
                                 {/* className=" "
                                 onClick={() => setSelectedPatientId(null)}
@@ -74,10 +75,10 @@ export const CardData = ({data}) => {
                                 {/* <EditExam patientId={selectedPatientId} onClose={() => setSelectedPatientId(null)} /> */}
                            {/* </button> */}
                             <button
-                                className=""
+                                className="transform transition hover:translate-y-1 hover:scale-105 shadow-lg rounded-full text-gray-800 border-2 text-sm p-1 md:text-base md:p-1.5 lg:text-base lg:p-1.5 xl:text-xl xl:font-bold xl:p-2 2xl:text-4xl 2xl:font-bold 2xl:p-6"
                                 onClick={() => setSelectedPatientId(null)}
                             >
-                                <AiOutlineClose className="text-gray-500 font-medium hover:text-gray-800 focus:outline-none text-base md:text-2xl xl:text-2xl 2xl:text-5xl" />
+                                <GrClose className="" />
                             </button>
                         </div>
 
