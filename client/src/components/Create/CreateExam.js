ab8a97e12942ba1ffb8f91a4bfa51a946bd5b3b6
Salmonte <almontesauel@gmail.com>
2023-03-15 00:44:26 -0400

    Updating componenets after pull and merge

--- client/src/components/Create/CreateExam.js
+++ client/src/components/Create/CreateExam.js
@@ -53,9 +53,9 @@ export const CreateExam = ({patientId}) => {
             <Navbar />  
             
             <div className="flex justify-center p-3 mt-28 md:p-4 md:mt-36 lg:p-6 lg:mt-32 xl:p-10 xl:mt-36 2xl:p-20 2xl:mt-36">
-                <form className="rounded-2xl shadow-2xl border-2 shadow-blue-900 bg-gray-100 w-full p-4 md:w-full md:p-6 lg:p-8 lg:w-full xl:p-10 2xl:p-16 2xl:w-full " action="" onSubmit={handleCreateExamSubmit}>
-                    <h1 className=" text-black font-bold text-base mb-2.5 md:text-xl md:mb-2 lg:text-2xl lg:mb-2 xl:text-3xl xl:mb-4 2xl:text-7xl 2xl:mb-8">Create Exam</h1>
-                    <hr className="border-b border-gray-300 2xl:border-b-4 2xl:mb-16" />   
+                <form className="rounded-2xl shadow-2xl border-2 shadow-blue-900 bg-gray-100 w-full p-4 md:w-full md:p-6 lg:p-8 lg:w-full xl:p-10 2xl:p-16 2xl:w-full" action="" onSubmit={handleCreateExamSubmit}>
+                    <h1 className=" text-black font-bold text-base mb-2.5 md:text-xl md:mb-2 lg:text-2xl lg:mb-2 xl:text-3xl xl:mb-3 2xl:text-7xl 2xl:mb-8">Create Exam</h1>
+                    <hr className="border-b border-gray-300 2xl:border-b-4 lg:mb-5 xl:mb-6 2xl:mb-16" />   
                         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-5 lg:gap-7 xl:gap-10 2xl:gap-24">
                             <div className="flex items-center xl: 2xl:justify-between" >
                                 <label className="w-1/2 text-base md:w-1/2 md:text-base lg:w-1/3 lg:text-xl xl:w-1/3 xl:text-2xl 2xl:w-1/2 2xl:text-6xl" htmlFor="patientId">Patient ID</label>
