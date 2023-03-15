796007bee10c3684df4336e8c8aca4d429ed0b51
Salmonte <almontesauel@gmail.com>
2023-03-15 01:00:50 -0400

    Ran Prettier

--- client/src/components/Cards/CardData.js
+++ client/src/components/Cards/CardData.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { GrClose } from "react-icons/gr";
-// import { AiOutlineEdit, AiOutlineClose } from "react-icons/ai";
 import Pagination from "../Pagination/Pagination";
 import { ExamDataPopUp } from "../PopUp/ExamDataPopUp";
 
