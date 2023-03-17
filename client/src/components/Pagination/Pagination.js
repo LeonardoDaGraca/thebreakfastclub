6c3bb066c55ed7907eb96c7c66cc8b9a35aabaf9
Salmonte <almontesauel@gmail.com>
2023-03-17 00:02:14 -0400

    Ran Prettier

--- client/src/components/Pagination/Pagination.js
+++ client/src/components/Pagination/Pagination.js
@@ -15,7 +15,7 @@ const Pagination = ({ totalPosts, postsPerPage, setCurrentPage }) => {
             <button
               key={index}
               onClick={() => setCurrentPage(page)}
-              className={"text-white focus:ring bg-blue-900 hover:bg-blue-800 rounded-lg m-0.5 px-1 py-1 text-xs md:mx-1 md:px-3 md:py-1 md:text-lg lg:mx-1 lg:px-3 lg:py-1 lg:text-xl xl:mx-1 xl:px-3.5 xl:py-1.5 xl:text-3xl 2xl:mx-2 2xl:px-8 2xl:py-4 2xl:text-5xl"}
+              className="text-white focus:ring bg-blue-900 hover:bg-blue-800 rounded mx-0.5 px-1 py-0.5 text-sm md:mx-1 md:px-3 md:py-1 md:text-lg lg:mx-1 lg:px-3 lg:py-1 lg:text-xl xl:mx-1 xl:px-3.5 xl:py-1.5 xl:text-3xl 2xl:mx-2 2xl:px-8 2xl:py-4 2xl:text-5xl"
             >
               {page}
             </button>
