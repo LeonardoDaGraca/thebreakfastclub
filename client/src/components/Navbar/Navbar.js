f4c56cd944c660df58c410902a351adcd16b0234
Salmonte <almontesauel@gmail.com>
2023-03-22 16:34:53 -0400

    Ran Prettier to tighten up code, fixed rows and columns when devices 760px or smaller

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -67,7 +67,7 @@ export const Navbar = ({ patientId, open }) => {
       </nav>
       {isOpen && (
         <div className="md:hidden absolute top-0 right-0 w-full h-full mt-16 z-10">
-          <div className="flex flex-col items-end p-5 shadow-lg shadow-[#060957] bg-[#060957]  backdrop-blur-lg  ">
+          <div className="flex flex-col items-end p-5 shadow-lg shadow-[#060957] bg-[#060957] bg-opacity-95 backdrop-blur-lg  ">
             <NavLink
               onClick={handleOpenCreateExamModal}
               className="block px-2.5 py-1 rounded-md shadow-md shadow-[#060957] bg-[#ffffff] text-[#060957] text-sm font-bold  transition transform ease-in-out hover:scale-105 md:ease-in-out delay-150 hover:-translate-y-1 md:hover:scale-105 2xl:hover:scale-110 duration-300 hover:font-bold"
