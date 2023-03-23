2ef15bac40e56cbc6d32a1b54d1af289ee3c4bfd
Salmonte <almontesauel@gmail.com>
2023-03-22 22:14:04 -0400

    Compressed tailwindcss

--- client/src/components/Login/Login2.js
+++ client/src/components/Login/Login2.js
@@ -6,9 +6,20 @@ import { GrClose } from "react-icons/gr";
 export const  Login2 = ({patientId, open, onClose}) => {
   return (
     <>
-        <div className="fixed flex flex-col justify-center items-center top-0 left-0 w-full h-full mx-auto border-2 ">
-            <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 max-w-xs md:max-w-2xl xl::max-w-4xl xl:p-0 ">
-                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <div 
+            className="fixed flex flex-col justify-center items-center top-0 left-0 w-full h-full mx-auto bg-[#060957] ">
+              <div
+                className="border bg-white  rounded-lg p-4 mb-5 md:mb-6"
+              >
+                <h1 
+                className="text-[#060957] font-bold text-base md:text-3xl 2xl:text-5xl"
+                >
+                    Welcome to TBC Health
+                </h1>
+              </div>
+              <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 max-w-xs md:max-w-2xl xl::max-w-4xl 2xl:max-w-6xl xl:p-0 ">
+                  
+                <div className="border p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
                         Sign in to your account
                     </h1>
